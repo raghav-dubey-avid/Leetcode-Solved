60dd161b3f813e61aebabf28d273173a522faaff
Raghavendra Dubey <36820299+raghav-dubey-avid@users.noreply.github.com>
2021-08-18 00:50:13 +0530

    Add files via upload

---
+++ LeetCode_Solved/Jump_Game-2.cpp
@@ -0,0 +1,40 @@
+/*
+
+Problem Statement : Given an array of non-negative integers nums, you are initially                     positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Your goal is to reach the last index in the minimum number of jumps.
+
+You can assume that you can always reach the last index.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: 2
+Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+
+Problem Link: https://leetcode.com/problems/jump-game-ii/
+
+*/
+
+
+class Solution {
+public:
+    int jump( vector<int>& nums ) {
+        
+        int n = size(nums), i = 0, maxReachable = 0, lastJumpedPos = 0, jumps = 0;
+	    while( lastJumpedPos < n - 1 ) {  
+            maxReachable = max( maxReachable, i + nums[i] );   
+            if( i == lastJumpedPos ) {			
+                lastJumpedPos = maxReachable;     
+                jumps++;                        
+            }            
+		    i++;
+	    }
+	    return jumps;
+    }
+};
